b8f808be01d9d532a76bd826f452d9d0a39d69f1
eadali <erkanadali91@gmail.com>
2019-08-29 00:36:14 +0300

    implement update

--- models.py
+++ models.py
@@ -6,25 +6,37 @@ Created on Thu Aug 22 22:36:20 2019
 @author: eadali
 """
 
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+
+
 from scipy.integrate import odeint
-import numpy
+from numpy import sin, copy, zeros, float32, pad
+
 from matplotlib import pyplot
 
 
 
 
 class pendulum_model:
-    def __init__(self, b, c, x_0):
+    def __init__(self, m, l, b, g, x_0):
         """Inits pendulum constants and initial state
 
         # Arguments
-            b: Pendulum constant 1
-            c: Pendulum constant 2
-            x_0: Initial state
+            m: Pendulum mass
+            l: Pendulum length
+            b: Pendulum friction coeff
+            g: Earth gravity acceleration
+            x_0: Pendulum initial state
         """
 
+        self.m = m
+        self.l = l
         self.b = b
-        self.c = c
+        self.g = g
+
         self.x_0 = x_0
 
 
@@ -41,9 +53,14 @@ class pendulum_model:
             Derivative of internal states
         """
 
+        # Calculates equation coeffs
+        c_1 = -self.b/(self.m*self.l**2)
+        c_2 = -self.g/self.l
+        c_3 = 1.0/(self.m*self.l**2)
+
         # ODE of pendulum
         theta, omega = x
-        dxdt = [omega, -self.b*omega - self.c*numpy.sin(theta)+u]
+        dxdt = [omega, c_1*omega + c_2*sin(theta) + c_3*u]
 
         return dxdt
 
@@ -68,21 +85,55 @@ class pendulum_model:
 
 
 
+
 class mass_spring_damper_model:
     def __init__(self, m, k, b, x_0):
+        """Inits pendulum constants and initial state
+
+        # Arguments
+            m: Mass
+            k: Spring coeff
+            b: Friction coeff
+            x_0: Initial state
+        """
+
         self.m = m
         self.k = k
         self.b = b
         self.x_0 = x_0
 
-    def ode(self, x, t, u):
-        pos, acc = x
 
+
+    def ode(self, x, t, u):
+        """Dynamic equations of pendulum
+
+        # Arguments
+            x: [position of mass, velocity of mass]
+            t: Time steps for ode solving
+            u: External force applied to the mass
+
+        # Returns
+            Derivative of internal states
+        """
+
+        # ODE of mass-spring-damper model
+        pos, acc = x
         dxdt = [acc, -(self.b/self.m)*acc - (self.k/self.m)*pos + (1/self.m)*u]
 
         return dxdt
 
+
+
     def update(self, u):
+        """Interface function for pendulum model
+
+        # Arguments
+            u: External force applied to the mass
+
+        # Returns
+            Position of mass
+        """
+
         # Solving ODE with scipy library
         x = odeint(self.ode, self.x_0, [0,0.1], args=(u,))
 
@@ -93,23 +144,141 @@ class mass_spring_damper_model:
 
 
 
+class lstm_model:
+    def __init__(self, model_shape, num_lookback, num_u, num_y):
+        self.x = zeros((1,num_lookback,num_u+num_y,))
+        self.model = Sequential()
+
+        num_layers = len(model_shape)
+
+        if self.equal(num_layers, 1):
+            num_cells = model_shape[0]
+            self.model.add(LSTM(num_cells, input_shape=(num_lookback,num_u+num_y)))
+
+        else:
+            num_cells = model_shape[0]
+            self.model.add(LSTM(num_cells, input_shape=(num_lookback,num_u+num_y),
+                                return_sequences=True))
+
+            for num_cells in model_shape[1:-1]:
+                self.model.add(LSTM(num_cells, return_sequences=True))
+
+            num_cells = model_shape[-1]
+            self.model.add(LSTM(num_cells))
+
+        self.model.add(Dense(num_y))
+        self.model.compile(loss='mse', optimizer='adam')
+
+        self.model.summary()
+
+
+
+    def equal(self, val_1, val_2):
+        condition_1 = (val_1 > (val_2-0.0001))
+        condition_2 = (val_1 < (val_2+0.0001))
+
+        return condition_1 and condition_2
+
+
+
+    def _reshape(self, x_data, y_data):
+        x_data = copy(x_data)
+        y_data = copy(y_data)
+
+
+
+        _, num_lookback, num_x = self.model.layers[0].input_shape
+        _, num_y = self.model.layers[-1].output_shape
+
+        new_shape = (x_data.shape[0], x_data.shape[1]-num_lookback,
+                     num_lookback, num_x)
+        x_data_new = zeros(new_shape, dtype=float32)
+
+        x_data = x_data[:,1:,]
+
+        for time_index in range(x_data_new.shape[1]):
+            x_data_new[:,time_index,:,0:num_x-num_y] = x_data[:,time_index:time_index+num_lookback]
+            x_data_new[:,time_index,:,num_x-num_y:num_x] = y_data[:,time_index:time_index+num_lookback]
+
+
+        y_data_new = y_data[:,num_lookback:,]
+
+        pyplot.subplot(2,1,1)
+        pyplot.plot(x_data_new[0,:,3,0], '.-b', label='input_signal')
+        pyplot.grid()
+
+        # Plot one output signal
+        pyplot.subplot(2,1,2)
+        pyplot.plot(x_data_new[0,:,3,1], '.-b', label='input_signal')
+        pyplot.plot(y_data_new[0,:], '.-r', label='output_signal')
+        pyplot.grid()
+
+        pyplot.show()
+
+        print(x_data_new.shape)
+        print(y_data_new.shape)
+        return x_data_new, y_data_new
+
+
+
+    def fit(self, x_data, y_data, validation_split=0.2, path='temp_model.h5'):
+        x_data = copy(x_data)
+        y_data = copy(y_data)
+
+        x_data, y_data = self._reshape(x_data, y_data)
+
+        _, num_lookback, num_x = self.model.layers[0].input_shape
+        _, num_y = self.model.layers[-1].output_shape
+
+        x_data = x_data.reshape(-1, num_lookback, num_x)
+        y_data = y_data.reshape(-1)
+
+
+        # Trains LSTM model
+        checkpoint = ModelCheckpoint(path, save_best_only=True)
+        self.model.fit(x_data, y_data, epochs=256,
+                       verbose=1, validation_split=validation_split,
+                       callbacks=[checkpoint,])
+
+        self.lstm_model = load_model(path)
+
+
+
+    def load(self, path):
+        self.lstm_model = load_model(path)
+
+
+
+    def update(self, u):
+        self.x[0,-1,0:] = u
+        y_pred = self.model.predict(self.x)
+
+        self.x[0,-1,1:] = y_pred
+
+        return y_pred[0]
+
+
+
+
+
+
 
 if __name__ == '__main__':
     """Test of models
     """
 
     # Test of pendulum_model class
-#    pendulum = pendulum_model(b=0.25, c=5, x_0=[1,0])
-#    theta = list()
-#
-#    for t in range(512):
-#        theta.append(pendulum.update(0.4))
-#
-#    pyplot.plot(theta, label='theta(t)')
-#    pyplot.legend(loc='best')
-#    pyplot.xlabel('t')
-#    pyplot.grid()
-#    pyplot.show()
+    pendulum = pendulum_model(m=1, l=1, b=0.25, g=9.8, x_0=[1,0])
+    theta = list()
+
+    for t in range(512):
+        theta.append(pendulum.update(8))
+
+    pyplot.plot(theta, label='theta(t)')
+    pyplot.legend(loc='best')
+    pyplot.xlabel('t')
+    pyplot.grid()
+    pyplot.show()
 
     # Test of mass_spring_damper_model class
     msd = mass_spring_damper_model(m=1, k=8, b=0.8, x_0=[1,0])
--- train_lstm.py
+++ train_lstm.py
@@ -6,13 +6,8 @@ Created on Thu Aug 22 22:36:20 2019
 @author: eadali
 """
 
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
-from keras.models import load_model
-from models import mass_spring_damper_model
-from numpy import cumsum, zeros, random, pad, float32
+from models import mass_spring_damper_model, lstm_model
+from numpy import cumsum, zeros, random, float32
 from matplotlib import pyplot
 
 
@@ -26,8 +21,8 @@ from matplotlib import pyplot
 # Data generation parameters
 split_ratio = 0.2
 
-num_samples = 128
-num_timesteps = 128
+num_samples = 1024
+num_timesteps = 64
 num_lookback = 4
 
 
@@ -40,7 +35,7 @@ x_0 = [0, 0]
 print('Generating data from dynamic pendulum model...')
 
 # Generates input data
-x_data = cumsum(random.rand(num_samples,num_timesteps)-0.5, axis=1)
+x_data = cumsum(random.rand(num_samples,num_timesteps,1)-0.5, axis=1)
 y_data = zeros(x_data.shape, dtype=float32)
 
 # Calculates output data with input data and dynamic model
@@ -51,129 +46,164 @@ for sample_index, input_signal in enumerate(x_data):
     for time_index, u in enumerate(input_signal):
         y_data[sample_index, time_index] = msd.update(u)
 
-# Reshape data for LSTM model
-x_data_temp = zeros((x_data.shape[0], x_data.shape[1], num_lookback, 2), dtype=float32)
-
-x_data = pad(x_data, ((0,0),(num_lookback-1,0)),
-                            'constant', constant_values=0.0)
-
-y_data = pad(y_data, ((0,0),(num_lookback,0)),
-                            'constant', constant_values=0.0)
-
-
-for time_index in range(x_data_temp.shape[1]):
-    x_data_temp[:,time_index,:,0] = x_data[:,time_index:time_index+num_lookback]
-    x_data_temp[:,time_index,:,1] = y_data[:,time_index:time_index+num_lookback]
-
-
-x_data = x_data_temp
-y_data = y_data[:,num_lookback:]
-
-#x_data = x_data_temp.reshape(-1, num_lookback, num_features)
-#y_data = y_data[:,num_lookback:].reshape(-1)
-
-
-# Split data to training, validation and test
-test_index = int(num_samples * split_ratio)
-val_index = test_index * 2
-
-x_test = x_data[:test_index]
-y_test = y_data[:test_index]
-x_val = x_data[test_index:val_index]
-y_val = y_data[test_index:val_index]
-x_train = x_data[val_index:]
-y_train = y_data[val_index:]
-
-
-print('x_train.shape..:', x_train.shape)
-print('y_train.shape..:', y_train.shape)
-print('x_val.shape..:', x_val.shape)
-print('y_val.shape..:', y_val.shape)
-print('x_test.shape', x_test.shape)
-print('y_test.shape', y_test.shape)
+print('x_data.shape..:', x_data.shape)
+print('y_data.shape..:', y_data.shape)
 
 
 # Plot one input signal
 pyplot.subplot(2,1,1)
-pyplot.plot(x_test[0,:,3,0], '.-b', label='input_signal')
+pyplot.plot(x_data[0,], '.-b', label='u(t)')
+pyplot.legend(loc='best')
 pyplot.grid()
 
 # Plot one output signal
 pyplot.subplot(2,1,2)
-pyplot.plot(x_test[0,:,3,1], '.-b', label='input_signal')
-pyplot.plot(y_test[0,:], '.-r', label='output_signal')
+pyplot.plot(y_data[0,], '.-r', label='y(t)')
+pyplot.legend(loc='best')
 pyplot.grid()
 
 pyplot.show()
 
 
 
+lstm = lstm_model([8,4], 4, 1, 1)
 
-# =============================================================================
-# CREATES LSTM MODEL
-# =============================================================================
-num_timesteps = x_train.shape[2]
-num_lookback = x_train.shape[3]
-
-# Creates LSTM model
-lstm_model = Sequential()
-lstm_model.add(LSTM(8, input_shape=(num_timesteps,num_lookback), return_sequences=True))
-lstm_model.add(LSTM(4))
-
-lstm_model.add(Dense(1))
-lstm_model.compile(loss='mse', optimizer=Adam())
-
-lstm_model.summary()
+lstm.fit(x_data,y_data)
 
 
 
 
-# =============================================================================
-# TRAINS LSTM MODEL
-# =============================================================================
-num_lookback = x_train.shape[2]
-num_features = x_train.shape[3]
 
-# Reshape training data for LSTM model
-x_train = x_train.reshape(-1, num_lookback, num_features)
-y_train = y_train.reshape(-1)
-x_val = x_val.reshape(-1, num_lookback, num_features)
-y_val = y_val.reshape(-1)
-
-print(x_train.shape)
-print(y_train.shape)
-
-# Trains LSTM model
+## Reshape data for LSTM model
+#x_data_temp = zeros((x_data.shape[0], x_data.shape[1], num_lookback, 2), dtype=float32)
+#
+#x_data = pad(x_data, ((0,0),(num_lookback-1,0)),
+#                            'constant', constant_values=0.0)
+#
+#y_data = pad(y_data, ((0,0),(num_lookback,0)),
+#                            'constant', constant_values=0.0)
+#
+#
+#for time_index in range(x_data_temp.shape[1]):
+#    x_data_temp[:,time_index,:,0] = x_data[:,time_index:time_index+num_lookback]
+#    x_data_temp[:,time_index,:,1] = y_data[:,time_index:time_index+num_lookback]
+#
+#
+#x_data = x_data_temp
+#y_data = y_data[:,num_lookback:]
+#
+##x_data = x_data_temp.reshape(-1, num_lookback, num_features)
+##y_data = y_data[:,num_lookback:].reshape(-1)
+#
+#
+## Split data to training, validation and test
+#test_index = int(num_samples * split_ratio)
+#val_index = test_index * 2
+#
+#x_test = x_data[:test_index]
+#y_test = y_data[:test_index]
+#x_val = x_data[test_index:val_index]
+#y_val = y_data[test_index:val_index]
+#x_train = x_data[val_index:]
+#y_train = y_data[val_index:]
+#
+#
+#print('x_train.shape..:', x_train.shape)
+#print('y_train.shape..:', y_train.shape)
+#print('x_val.shape..:', x_val.shape)
+#print('y_val.shape..:', y_val.shape)
+#print('x_test.shape', x_test.shape)
+#print('y_test.shape', y_test.shape)
+#
+#
+## Plot one input signal
+#pyplot.subplot(2,1,1)
+#pyplot.plot(x_test[0,:,3,0], '.-b', label='input_signal')
+#pyplot.grid()
+#
+## Plot one output signal
+#pyplot.subplot(2,1,2)
+#pyplot.plot(x_test[0,:,3,1], '.-b', label='input_signal')
+#pyplot.plot(y_test[0,:], '.-r', label='output_signal')
+#pyplot.grid()
+#
+#pyplot.show()
+#
+#
+#
+#
+## =============================================================================
+## CREATES LSTM MODEL
+## =============================================================================
+#num_timesteps = x_train.shape[2]
+#num_lookback = x_train.shape[3]
+#
+## Creates LSTM model
+#lstm_model = Sequential()
+#lstm_model.add(LSTM(8, input_shape=(num_timesteps,num_lookback), return_sequences=True))
+#lstm_model.add(LSTM(4))
+#
+#lstm_model.add(Dense(1))
+#lstm_model.compile(loss='mse', optimizer=Adam())
+#
+#lstm_model.summary()
+#
+#
+#
+#
+## =============================================================================
+## TRAINS LSTM MODEL
+## =============================================================================
+#num_lookback = x_train.shape[2]
+#num_features = x_train.shape[3]
+#
+## Reshape training data for LSTM model
+#x_train = x_train.reshape(-1, num_lookback, num_features)
+#y_train = y_train.reshape(-1)
+#x_val = x_val.reshape(-1, num_lookback, num_features)
+#y_val = y_val.reshape(-1)
+#
+#print(x_train.shape)
+#print(y_train.shape)
+#
+## Trains LSTM model
 #checkpoint = ModelCheckpoint('lstm_model.h5', save_best_only=True)
-#lstm_model.fit(x_train, y_train, epochs=256,
-#               verbose=1, validation_data=(x_val, y_val),
-#               callbacks=[checkpoint,])
-
-
-# Loads LSTM model
-lstm_model = load_model('lstm_model.h5')
-# Predicts model output signal
+##lstm_model.fit(x_train, y_train, epochs=256,
+##               verbose=1, validation_data=(x_val, y_val),
+##               callbacks=[checkpoint,])
+#
+#
+## Loads LSTM model
+#lstm_model = load_model('lstm_model.h5')
+## Predicts model output signal
 #x_test[:,:,:,1] = 0.0
 #y_pred = zeros(y_test.shape[1],float32)
 #
-#for time_index, x in enumerate(x_test[0,]):
-#    if time_index
-#    y_pred[time_index] =
-
 #
-y_pred = lstm_model.predict(x_test[0,].reshape(-1,num_lookback, num_features))
-
-# Plot Results
+#
+#for time_index in range(x_test.shape[1]):
+#    y_pred[time_index] = lstm_model.predict(x_test[0:1,time_index])
+#    if time_index < 64-4:
+#        x_test[0,time_index+1,3,1] = y_pred[time_index]
+#        x_test[0,time_index+2,2,1] = y_pred[time_index]
+#        x_test[0,time_index+3,1,1] = y_pred[time_index]
+#        x_test[0,time_index+4,0,1] = y_pred[time_index]
+#
+#
+#
+##
+##y_pred = lstm_model.predict(x_test[0,].reshape(-1,num_lookback, num_features))
+#
+## Plot Results
+##pyplot.subplot(2,1,1)
+##pyplot.plot(x_test[0,:])
+##pyplot.grid()
+#
 #pyplot.subplot(2,1,1)
-#pyplot.plot(x_test[0,:])
+#pyplot.plot(x_test[0,:,3,0], '.-b', label='input_signal')
 #pyplot.grid()
-
-pyplot.subplot(2,1,1)
-pyplot.plot(x_test[0,:,3,0], '.-b', label='input_signal')
-pyplot.grid()
-
-pyplot.subplot(2,1,2)
-pyplot.plot(y_test[0,:], '.-r', label='output_signal')
-pyplot.plot(y_pred)
-
-pyplot.show()
+#
+#pyplot.subplot(2,1,2)
+#pyplot.plot(y_test[0,:], '.-r', label='output_signal')
+#pyplot.plot(y_pred)
+#pyplot.show()
